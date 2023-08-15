9b241b515bc6800513a14d3572ff1688187833c4
Pavan Kumar <pavanmaddala16@gmail.com>
2023-08-15 14:15:59 +0530

    Added crud operations to products table

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,25 +1,56 @@
 const { Product } = require("../model");
-
-async function getProduct(req, res) {
-  res.json("Product");
-}
+const product = require("../model/product");
 
 async function getProducts(req, res) {
-  const products = await Product.findAll();
+  const products = await Product.findAll({});
 
   res.json({ status: true, products });
 }
 
+async function getProduct(req, res) {
+  try {
+    const product = await Product.findOne({ where: { id: req.params.id } })
+
+    if (!product) return res.status(404).json({ status: false, message: "Product not found!" });
+
+    res.json({ status: true, product });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: false, error: error.message });
+  }
+}
+
 async function createProduct(req, res) {
-  res.json("Product Created");
+  try {
+    const product = await Product.create(req.body);
+
+    res.json({ status: true, product });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: false, error: error.message });
+  }
 }
 
 async function updateProduct(req, res) {
-  res.json("Product Updated");
+  try {
+    const product = await Product.update(req.body, { where: { id: req.params.id } });
+
+    res.json({ status: true, product });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: false, error: error.message });
+  }
 }
 
 async function deleteProduct(req, res) {
-  res.json("Product Deleted");
+  try {
+    const product = await Product.destroy({ where: { id: req.params.id } });
+    
+    res.json({ status: true, product });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: false, error: error.message });
+  }
 }
 
 module.exports = {
